4f5a5bf8488a374842d576ad6d6299e769b20a6c
Frank C <fac3d@att.net>
2018-03-24 08:40:48 -0700

    Add files via upload
    Added two python files.

--- python/test.py
+++ python/test.py
@@ -1,1 +1,24 @@
+#!/usr/bin/python
+
+from sense_hat import SenseHat
+import time
+
+sense = SenseHat()
+
+while True:
+    pitch, roll, yaw = sense.get_orientation().values()
+    t = sense.get_temperature()
+    p = sense.get_pressure()
+    h = sense.get_humidity()
+
+    t = round(t, 1)
+    p = round(p, 1)
+    h = round(h, 1)
+
+    pitch=round(pitch, 2)
+    roll=round(roll, 2)
+    yaw=round(yaw, 2)
+
+    print("pitch=%s, roll=%s, yaw=%s t=%s, p=%s, h=%s" % (pitch, yaw, roll, t, p, h))
+    time.sleep(0.5)
 
