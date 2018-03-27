ffa8b962e7b71a7b1e34291a216631191205d341
Frank C <fac3d@att.net>
2018-03-26 19:03:32 -0700

    Add files via upload

--- python/air-schedule.py
+++ python/air-schedule.py
@@ -12,28 +12,40 @@ import sqlite3
 # Functions
 def opendb():
     conn=sqlite3.connect('air-schedule.db')
-    conn.execute('''CREATE TABLE SCHEDULE(ID INT PRIMARY KEY NOT NULL, FLTNUM TEXT, DEPART TEXT, ARRIVE TEXT, DURATION INT);''')
+    conn.execute('''CREATE TABLE SCHEDULE(ID INT PRIMARY KEY NOT NULL, \
+                    FLTNUM TEXT, DEPART_CITY TEXT, ARRIVE_CITY TEXT, 
+                    DEPART_TIME TEXT, ARRIVE_TIME TEXT, DURATION INT);''')
     conn.close()
 
 def insertdb():
     conn=sqlite3.connect('air-schedule.db')
-    conn.execute("INSERT INTO SCHEDULE(ID, FLTNUM, DEPART, ARRIVE, DURATION) VALUES(1, '741', 'HNL', 'LIH', 35)");
-    conn.execute("INSERT INTO SCHEDULE(ID, FLTNUM, DEPART, ARRIVE, DURATION) VALUES(2, '742', 'LIH', 'HNL', 35)");
-    conn.execute("INSERT INTO SCHEDULE(ID, FLTNUM, DEPART, ARRIVE, DURATION) VALUES(3, '743', 'HNL', 'OGG', 30)");
-    conn.execute("INSERT INTO SCHEDULE(ID, FLTNUM, DEPART, ARRIVE, DURATION) VALUES(4, '744', 'OGG', 'HNL', 30)");
+    conn.execute("INSERT INTO SCHEDULE(ID, FLTNUM, DEPART_CITY, ARRIVE_CITY, \
+                  DEPART_TIME, ARRIVE_TIME, DURATION) VALUES(1, '741', 'HNL', 'LIH', '23-MAR-18 0640', '23-MAR-18 0740', 35)");
+    conn.execute("INSERT INTO SCHEDULE(ID, FLTNUM, DEPART_CITY, ARRIVE_CITY, \
+                  DEPART_TIME, ARRIVE_TIME, DURATION) VALUES(2, '742', 'LIH', 'HNL', '23-MAR-18 0640', '23-MAR-18 0740', 35)");
+    conn.execute("INSERT INTO SCHEDULE(ID, FLTNUM, DEPART_CITY, ARRIVE_CITY, \
+                  DEPART_TIME, ARRIVE_TIME, DURATION) VALUES(3, '743', 'HNL', 'OGG', '23-MAR-18 0740', '23-MAR-18 0840', 30)");
+    conn.execute("INSERT INTO SCHEDULE(ID, FLTNUM, DEPART_CITY, ARRIVE_CITY, \
+                  DEPART_TIME, ARRIVE_TIME, DURATION) VALUES(4, '744', 'OGG', 'HNL', '23-MAR-18 0740', '23-MAR-18 0840', 30)");
     conn.commit()
     conn.close()
 
 def readdb():
     conn=sqlite3.connect('air-schedule.db')
-    cursor=conn.execute("SELECT ID, FLTNUM, DEPART, ARRIVE, DURATION FROM SCHEDULE")
+    cursor=conn.execute("SELECT ID, FLTNUM, DEPART_CITY, ARRIVE_CITY, DEPART_TIME, ARRIVE_TIME, DURATION FROM SCHEDULE")
 
     for row in cursor:
         print ("ID=", row[0])
         print ("FLTNUM=", row[1])   
-        print ("DEPART=", row[2])   
-        print ("ARRIVE=", row[3])
-        print ("DURATION=", row[4])   
+        print ("DEPART_CITY=", row[2])   
+        print ("ARRIVE_CITY=", row[3])
+        print ("DEPART_TIME=", row[4])   
+        print ("ARRIVE_TIME=", row[5])
+        print ("DURATION=", row[6])   
+
+    cursor=conn.execute("SELECT ID, FLTNUM, DEPART_CITY, ARRIVE_CITY, DEPART_TIME, ARRIVE_TIME, DURATION FROM SCHEDULE")
+    for row in cursor:
+       print(row[0], row[1], row[2], row[3], row[4], row[5], row[6])
 
     conn.close()
 #
