50ec91e8546ff6d83d030b034c2799b115021165
Frank C <fac3d@att.net>
2019-12-22 10:46:02 -0800

    Create csvReaderWriterMain.py
    Calls the functions in csvReaderWriter.py.

---
+++ python/csvReaderWriterMain.py
@@ -0,0 +1,15 @@
+#
+# Filename: csvReaderWriterMain.py
+#
+# Runs the functions in csvReaderWriter.py
+#
+# Date: 01-Dec-19
+#
+#
+import csvReaderWriter as csvRW
+
+csvRW.myWriter()
+csvRW.myReader()
+csvRW.myRowChecker()
+csvRW.testRowFunction()
+csvRW.multiplyTable()
